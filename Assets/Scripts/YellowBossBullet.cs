cc11c154f4e02eecbc7e0523be1099ef218b9336
Jennifer-Vazquez <vazquez.jennifer5@gmail.com>
2022-03-15 22:23:12 -0700

    pls

--- Assets/Scripts/YellowBossBullet.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class YellowBossBullet : MonoBehaviour
-{
-    void OnCollisionEnter(Collision collision)
-    {
-        if( !collision.gameObject.CompareTag("Player")) Destroy(gameObject);
-    }
-}
