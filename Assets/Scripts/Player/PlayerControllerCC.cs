f5bffe272306431926692dbdfddda5d9ef942b73
Jennifer-Vazquez <vazquez.jennifer5@gmail.com>
2022-03-16 17:22:12 -0700

    final touches i think

--- Assets/Scripts/Player/PlayerControllerCC.cs
+++ Assets/Scripts/Player/PlayerControllerCC.cs
@@ -234,7 +234,7 @@ public class PlayerControllerCC : MonoBehaviour
         _characterController.enabled = false;
         yield return new WaitForSeconds(0.1f);
         gameObject.transform.position = wormholePos2;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.5f);
         _characterController.enabled = true;
     }
 
@@ -243,7 +243,7 @@ public class PlayerControllerCC : MonoBehaviour
         _characterController.enabled = false;
         yield return new WaitForSeconds(0.1f);
         gameObject.transform.position = wormholePos1;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.5f);
         _characterController.enabled = true;
     }
 
